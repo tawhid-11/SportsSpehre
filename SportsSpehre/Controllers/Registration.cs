bbbf41a5c924ef12d4a1c24142bf26b0975b2172
Md.TauhidIslam <168570535+tawhid-11@users.noreply.github.com>
2025-12-15 16:53:56 +0600

    Initial commit

---
+++ SportsSpehre/Controllers/Registration.cs
@@ -0,0 +1,34 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SportsSpehre.Context;
+
+namespace SportsSpehre.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Registration : ControllerBase
+    {
+        private DapperContext _context;
+        private object registration;
+
+        public Registration(DapperContext context)
+        {
+            _context = context;
+        }
+        [HttpGet("GetAllUser")]
+        public async Task<IActionResult> GetAllUser()
+        {
+            try
+            {
+                var registration = await _context.CreateConnection().QueryAsync("SELECT * FROM Registration");
+                return Ok(registration);
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+    }
+    
+}
